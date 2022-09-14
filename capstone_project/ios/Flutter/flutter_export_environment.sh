3fa5d0d01d2027f75585cf1a43ec9058e056227f
DESKTOP-JFCQK15\User <s108213039@mail1.ncnu.edu.tw>
2022-09-14 18:17:46 +0800

    gitignore

--- capstone_project/ios/Flutter/flutter_export_environment.sh
+++ capstone_project/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Dart\capstone_project"
+export "FLUTTER_ROOT=D:\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Github\Capstone_Project\capstone_project"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
