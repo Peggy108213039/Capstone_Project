6ca84e8bc05150bd3287a08f6aa34378f8d44b7d
Huei-Lin-Lin <aaaaaa121516202326@gmail.com>
2022-11-27 16:20:14 +0800

    fix audio player

--- capstone_project/lib/services/audio_player.dart
+++ capstone_project/lib/services/audio_player.dart
@@ -10,12 +10,14 @@ class AudioPlayerService {
         await rootBundle.load(alarmAudioPath); //load audio from assets
     Uint8List audiobytes =
         bytes.buffer.asUint8List(bytes.offsetInBytes, bytes.lengthInBytes);
-    int result = await player.playBytes(audiobytes);
-    if (result == 1) {
-      //play success
-      print("audio is playing.");
-    } else {
-      print("Error while playing audio.");
+    if (player.state != PlayerState.PLAYING) {
+      int result = await player.playBytes(audiobytes);
+      if (result == 1) {
+        //play success
+        print("audio is playing.");
+      } else {
+        print("Error while playing audio.");
+      }
     }
     await player.resume();
   }
